b93ea3791ad0331912e5051e9b588cb4e6df996c
Josh Carey <josh@harbr.co>
2016-05-19 17:09:36 -0400

    starting on site shell (header, menu, footer)

--- header.php
+++ header.php
@@ -4,12 +4,23 @@
       <meta charset="<?php bloginfo('charset'); ?>">
       <title><?php wp_title(''); ?></title>
 
-      <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-      <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-114x114.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-144x144.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-152x152.png" />
+
+      <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-16x16.png" sizes="16x16" />
+      <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" />
+
+      <meta name="msapplication-TileColor" content="#221E1F" />
+      <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/img/icons/mstile-144x144.png" />
+
 
       <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
       <meta name="viewport" content="width=device-width,initial-scale=1.0">
       <meta name="description" content="<?php bloginfo('description'); ?>">
+
+      <link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic|Oswald:400,300,700' rel='stylesheet' type='text/css'>
 
       <?php wp_head(); ?>
 
@@ -36,3 +47,5 @@
          <!-- /header -->
 
       </div><!-- /.header-wrapper -->
+
+      <div class="middle-wrapper">
